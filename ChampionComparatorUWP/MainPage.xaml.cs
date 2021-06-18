de0a8c6ee178c2ee8e80ba885e11718e6c626efc
LeddaZ <leonardoledda@gmail.com>
2021-06-18 13:04:39 +0200

    Cleanup

--- ChampionComparatorUWP/MainPage.xaml.cs
+++ ChampionComparatorUWP/MainPage.xaml.cs
@@ -1,14 +1,13 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Net.Http;
 using Windows.ApplicationModel;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
