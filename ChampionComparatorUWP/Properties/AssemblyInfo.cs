7c0bbe9fb1a8c7c95a8c4c215acec64670e44cf9
LeddaZ <leonardoledda@gmail.com>
2021-07-04 19:54:42 +0200

    Revert target SDK to 19041
    * Fixes font not being applied

--- ChampionComparatorUWP/Properties/AssemblyInfo.cs
+++ ChampionComparatorUWP/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Resources;
+
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -27,3 +28,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
